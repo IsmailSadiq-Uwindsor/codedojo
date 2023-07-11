7175a868e9af2664c28118c8b5614839bf840ec7
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-07-10 22:51:22 -0400

    Reseting codebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import { Container } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-        <Outlet/>
-      </Container>
-    </main>
-    <Footer/>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { 
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HomeScreen from './screens/HomeScreen';
-import LearningPathScreen from './screens/LearningPathScreen';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route index={true} path='/' element={<HomeScreen/>}/>
-      <Route path='/learningPath/:id' element={<LearningPathScreen/>}/>
-    </Route>
-  )
-)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
