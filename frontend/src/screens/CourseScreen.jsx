b9148e77661ef892ac04af5cc200f696ebb8d557
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-07-14 15:44:58 -0400

    Serving the learning path, courses and quiz data from the APIs

--- frontend/src/screens/CourseScreen.jsx
+++ frontend/src/screens/CourseScreen.jsx
@@ -1,18 +1,39 @@
 import {useEffect, useState} from 'react'
-// import axios from 'axios';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
-import Course from '../components/Course'
-import courses from '../courses'
+// import Course from '../components/Course'
+// import courses from '../courses'
 import Quiz from '../components/Quiz'
-import quizzes from '../quizzes'
+// import quizzes from '../quizzes'
 
 const CourseScreen = () => {
 
-    const { courseId: courseId, learningPathId: learningPathId } = useParams ();
+    const { learningPathId: learningPathId, courseId: courseId } = useParams ();
+    const [course, setCourse] = useState({});
+    useEffect(() => {
+        const fetchCourse = async () => {
+          const courseAllData = await axios.get(`/api/learningPaths/${learningPathId}/courses/${courseId}`);
+          const courseData = courseAllData.data;
+          setCourse(courseData);
+        };
+        fetchCourse();
+      }, [learningPathId, courseId]);
 
-    const course = courses.find((qL) => qL._courseId === courseId && qL._learningPathId === learningPathId);
+      const [quizzes, setQuizzes] = useState([])
+      useEffect(() => {
+        const fetchQuizzes = async () => {
+          const coursesAllData  = await axios.get(`/api/learningPaths/${learningPathId}/courses/${courseId}/quizzes`);
+          const coursesData = coursesAllData.data;
+          setQuizzes(coursesData);
+        };
+        fetchQuizzes();
+      }, [learningPathId, courseId]);
+
+    // const { courseId: courseId, learningPathId: learningPathId } = useParams ();
+
+    // const course = courses.find((qL) => qL._courseId === courseId && qL._learningPathId === learningPathId);
 
     const [test, setTest] = useState({})
 
@@ -39,8 +60,8 @@ const CourseScreen = () => {
 
                 <h4>Quizzes: </h4>
                 <Row>
-                    {quizzes.filter(obj => obj._courseId == courseId).map( (quiz, index) => (
-                        //<Col key={course._id} sm={12} md={6} lg={4} xl={3}>
+                    {/* {quizzes.filter(obj => obj._courseId == courseId).map( (quiz, index) => ( */}
+                    {quizzes.map( (quiz, index) => (
                         <Row key={index} >
                             <Quiz courseId={courseId} quiz={quiz} test={test}/>
                         </Row>
