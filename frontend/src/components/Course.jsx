030be7e24529b60809be8736574fa68daea9e2e7
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-07-14 23:46:02 -0400

    Created functions to seed the database with Users, LearningPaths, Courses, and Quizzes

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Course = ({ learningPathId, course}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <Link to={`/learningPaths/${learningPathId}/courses/${course._courseId}/quizzes`}/>
+        <Link to={`/learningPaths/${learningPathId}/courses/${course._id}/quizzes`}/>
             {/* <Card.Img src={learningPath.image} variant="top"/> */}
         {/* </a> */}
         <Card.Body>
-            <Link to={`/learningPaths/${learningPathId}/courses/${course._courseId}/quizzes`}>
+            <Link to={`/learningPaths/${learningPathId}/courses/${course._id}/quizzes`}>
                 <Card.Title as='div' className='product-title'>
                     <strong>{course.title}</strong>
                 </Card.Title>
