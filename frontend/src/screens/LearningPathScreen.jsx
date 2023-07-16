ab5177707dcef0f4cd3796f1c1f384e022d83f7b
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-07-16 00:25:07 -0400

    Added functionality for Add To Cart button:
    - When clicked the LP gets added to local storage including price
    - If the LP id is in local storage Add To Cart button is disabled
    - You can add multiple LPs and price gets calculated in local storage

--- frontend/src/screens/LearningPathScreen.jsx
+++ frontend/src/screens/LearningPathScreen.jsx
@@ -1,14 +1,19 @@
-import { useParams, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import { useGetLearningPathDetailsQuery, useGetCoursesForLearningPathQuery } from '../slices/productsApiSlice';
 import Course from '../components/Course'
 import Rating from '../components/Rating';
 import Loader from "../components/Loader";
 import Message from '../components/Message'; 
+import { addToCart } from '../slices/cartSlice';
 
 const LearningPathScreen = () => {
 
     const { learningPathId: learningPathId} = useParams ();
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const learningPathData = useGetLearningPathDetailsQuery(learningPathId);
 
@@ -26,16 +31,35 @@ const LearningPathScreen = () => {
   
     const coursesError = coursesData.isError
 
+    const addToCartHandler = () => {
+      dispatch(addToCart({ ...learningPath}));
+      navigate('/cart')
+    }
+  
+    let local = JSON.parse(localStorage.getItem("cart"));
+    let button = false;
+    if(local !== null){
+    for (let i = 0; i < local.cartItems.length; i++){
+          if (local.cartItems[i]._id === learningPathId){
+            button = true
+          }
+      }
+    }
+  
   return (
     <>
         <Link className='btn btn-light my-3' to="/learningPaths">Go Back</Link>
 
+        
+
         {learningPathIsLoading ? (
           <Loader/>
         ) : learningPathError ? (<Message variant='danger'>{learningPathError?.data?.message || learningPathError.error}</Message>) : ( <>
-          <Button style = {{marginLeft: 20}} className='btn-block' type='button' disabled={learningPath.isActive === false} >
+        
+          <Button style = {{marginLeft: 20}} className='btn-block' type='button' disabled={learningPath.isActive === false || button === true} onClick={addToCartHandler}>
                                       Add To Cart
           </Button>
+
           <Row>
             <Col md={5}>
                 <ListGroup variant='flush'>
