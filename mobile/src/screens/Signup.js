ca8cd692dc4ab834b7fdf42d43c1ff7adda2c0b4
durga ketharaju <ketharal@uwindsor.ca>
2023-07-17 18:26:04 -0400

    -navigation

--- mobile/src/screens/Signup.js
+++ mobile/src/screens/Signup.js
@@ -1,10 +1,14 @@
-
 import React from 'react';
 import { View, TextInput, StyleSheet, Image, Text,Pressable} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 
 const Signup =()=>{
+  const navigation = useNavigation();
+  const handlesignup = () => {
+    navigation.navigate('LoginScreen');
+  };
+
   return(
       <Text>Signup Screen</Text>
   );
