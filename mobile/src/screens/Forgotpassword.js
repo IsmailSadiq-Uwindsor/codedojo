4f21e17bf158da56eec4aba451d865d67190e193
Pilla Nava Chandrika <pillan@uwindsor.ca>
2023-07-18 03:48:27 +0530

    Navigation

--- mobile/src/screens/Forgotpassword.js
+++ mobile/src/screens/Forgotpassword.js
@@ -4,6 +4,9 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
 
 const Forgotpassword =()=>{
+  const navigation = useNavigation();
+  const handlelink = () => {
+  };
   return(
       <Text>Forgot password Screen</Text>
   );
