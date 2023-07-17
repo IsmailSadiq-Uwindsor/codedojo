f8d30a0187ea7fd715a6fb7ce1db05b3873cc135
Pilla Nava Chandrika <pillan@uwindsor.ca>
2023-07-18 03:44:56 +0530

    - Imoorting necessary dependencies for the forgot password screen

--- mobile/src/screens/Forgotpassword.js
+++ mobile/src/screens/Forgotpassword.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { View, TextInput, StyleSheet, Image, Text,Pressable} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
 
 const Forgotpassword =()=>{
   return(
