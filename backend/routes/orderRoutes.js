7f2386032e0207a8432ddc47c5d478632b9d0b8f
Ismail Sadiq <sadiq6@uwindsor.ca>
2023-07-16 23:51:58 -0400

    Created order endpoints

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { 
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToAssigned,
+    getOrders
+ } from '../controllers/orderController.js';
+ import { protect, admin } from '../middleware/authMiddleware.js'
+
+const router = express.Router();
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+
+router.route('/mine').get(protect, getMyOrders);
+
+router.route('/:orderId').get(protect, admin, getOrderById);
+
+router.route('/:orderId/pay').put(protect, updateOrderToPaid);
+
+router.route('/:orderId/assign').put(protect, admin, updateOrderToAssigned);
+
+export default router;
